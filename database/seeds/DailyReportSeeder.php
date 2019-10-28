e7c277511e1138ff2d30c2e5bc80a5dd90d15ffe
vagrant <vagrant@homestead>
2019-10-28 23:11:01 +0000

    テストデータを追加(->3)

--- database/seeds/DailyReportSeeder.php
+++ database/seeds/DailyReportSeeder.php
@@ -13,12 +13,30 @@ class DailyReportSeeder extends Seeder
     {
         DB::table('daily_reports')->truncate();
         DB::table('daily_reports')->insert([
-            'user_id' => 1,
-            'title' => 'テストです',
-            'content' => 'テストです',
-            'reporting_time' => Carbon::create(2018,7,1,13,00,30),
-            'created_at' => Carbon::create(2019,7,1,13,00,30),
-            'updated_at' => Carbon::create(2019,7,2,12,00,00),
+            [
+                'user_id' => 4,
+                'title' => 'テスト',
+                'content' => 'テスト',
+                'reporting_time' => Carbon::create(2019,7,1,13,00,30),
+                'created_at' => Carbon::create(2019,7,1,13,00,30),
+                'updated_at' => Carbon::create(2019,7,2,12,00,00),
+            ],
+            [
+                'user_id' => 4,
+                'title' => 'test',
+                'content' => 'test',
+                'reporting_time' => Carbon::create(2018,8,1,13,00,30),
+                'created_at' => Carbon::create(2019,8,1,13,00,30),
+                'updated_at' => Carbon::create(2019,8,2,12,00,00),
+            ],
+            [
+                'user_id' => 4,
+                'title' => 'testest',
+                'content' => 'testest',
+                'reporting_time' => Carbon::create(2018,9,1,13,00,30),
+                'created_at' => Carbon::create(2019,9,1,13,00,30),
+                'updated_at' => Carbon::create(2019,9,2,12,00,00),
+            ]
         ]);
     }
 }
