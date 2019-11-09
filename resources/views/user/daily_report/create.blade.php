d1b578b775e4249ed560f028a254176ff05f4f78
vagrant <vagrant@homestead>
2019-11-09 23:33:02 +0000

    バリデーションエラー時のCSS追加（has-error）及びカンマ等修正

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -4,19 +4,19 @@
 <h2 class="brand-header">日報作成</h2>
 <div class="main-wrap">
   <div class="container">
-    {{ Form::open(['route' => 'dailyreport.store', ]) }}
+    {{ Form::open(['route' => 'dailyreport.store' ]) }}
       <div class="form-group form-size-small">
-        {{ Form::input('date', 'reporting_time',null, ['class' => 'form-control']) }}
+        {{ Form::input('date', 'reporting_time', null, ['class' => 'form-control']) }}
         <span class="help-block"></span>
       </div>
-      <div class="form-group">
+      <div class="form-group @if(!empty($errors->first('title'))) has-error @endif">
         {{ Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => 'Title']) }}
         @if($errors->has('title'))
-        <span class="help-block">{{ $errors->first('title') }}</span>
+        <span class="has-error help-block">{{ $errors->first('title') }}</span>
         @endif
       </div>
-      <div class="form-group">
-        {{ Form::textarea('content', null, ['class' => 'form-control','placeholder' => 'Content']) }}
+      <div class="form-group  @if(!empty($errors->first('content'))) has-error @endif">
+        {{ Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Content']) }}
         @if($errors->has('content'))
         <span class="help-block">{{ $errors->first('content') }}</span>
         @endif
