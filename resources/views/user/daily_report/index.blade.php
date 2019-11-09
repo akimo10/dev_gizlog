b9ab280798cf3c760c278f3b92cb949eb26815ab
vagrant <vagrant@homestead>
2019-11-09 23:40:50 +0000

    日報一覧表示の改修

--- resources/views/user/daily_report/index.blade.php
+++ resources/views/user/daily_report/index.blade.php
@@ -4,12 +4,11 @@
 <h2 class="brand-header">日報一覧</h2>
 <div class="main-wrap">
   <div class="btn-wrapper daily-report">
-    <form action="{{ route('dailyreport.index') }}" method="get">
-      @CSRF
-      <input type="hidden" name="select" value="search">
-      <input class="form-control" name="search-month" type="month">
+    {{ Form::open(['route' => 'dailyreport.index', 'method' => 'get']) }}
+      {{ Form::input('hidden', 'select', 'search') }}
+      {{ Form::input('month', 'search-month', null, ['class' => 'form-control']) }}
       <button type="submit" class="btn btn-icon"><i class="fa fa-search"></i></button>
-    </form>
+    {{ Form::close() }}
     <a class="btn btn-icon" href="{{ route('dailyreport.create') }}"><i class="fa fa-plus"></i></a>
   </div>
   <div class="content-wrapper table-responsive">
@@ -22,16 +21,17 @@
           <th class="col-xs-2"></th>
         </tr>
       </thead>
-      @foreach($reports as $dailyReport)
       <tbody>
+        @foreach($reports as $dailyReport)
           <tr class="row">
             <td class="col-xs-2">{{ $dailyReport->reporting_time }}</td>
             <td class="col-xs-3">{{ $dailyReport->title }}</td>
             <td class="col-xs-5">{{ $dailyReport->content }}</td>
             <td class="col-xs-2"><a class="btn" href="{{ route('dailyreport.show',$dailyReport->id) }}"><i class="fa fa-book"></i></a></td>
           </tr>
+        @endforeach
       </tbody>
-      @endforeach
+
     </table>
   </div>
 </div>
