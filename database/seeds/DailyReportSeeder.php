c396ee38dff247703638340669e56b7e608998c1
vagrant <vagrant@homestead>
2019-11-12 03:53:16 +0000

    コメントアウト削除及びuseに伴う記述修正

--- database/seeds/DailyReportSeeder.php
+++ database/seeds/DailyReportSeeder.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-// use App\Models\DailyReport;
-// use Illuminate\Database\Eloquent\Model;
+use App\Models\DailyReport;
 
 class DailyReportSeeder extends Seeder
 {
@@ -14,32 +13,6 @@ class DailyReportSeeder extends Seeder
     public function run()
     {
         DB::table('daily_reports')->truncate();
-        factory(App\Models\DailyReport::class,10)->create();
-        // DB::table('daily_reports')->insert([
-        //     [
-        //         'user_id' => 4,
-        //         'title' => 'テスト',
-        //         'content' => 'テスト',
-        //         'reporting_time' => Carbon::create(2019,7,1,13,00,30),
-        //         'created_at' => Carbon::create(2019,7,1,13,00,30),
-        //         'updated_at' => Carbon::create(2019,7,2,12,00,00),
-        //     ],
-        //     [
-        //         'user_id' => 4,
-        //         'title' => 'test',
-        //         'content' => 'test',
-        //         'reporting_time' => Carbon::create(2018,8,1,13,00,30),
-        //         'created_at' => Carbon::create(2019,8,1,13,00,30),
-        //         'updated_at' => Carbon::create(2019,8,2,12,00,00),
-        //     ],
-        //     [
-        //         'user_id' => 4,
-        //         'title' => 'testest',
-        //         'content' => 'testest',
-        //         'reporting_time' => Carbon::create(2018,9,1,13,00,30),
-        //         'created_at' => Carbon::create(2019,9,1,13,00,30),
-        //         'updated_at' => Carbon::create(2019,9,2,12,00,00),
-        //     ]
-        // ]);
+        factory(DailyReport::class,10)->create();
     }
 }
