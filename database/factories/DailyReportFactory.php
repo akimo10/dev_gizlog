b0e678afa98877c1f9566d4332b6416f26a9360d
vagrant <vagrant@homestead>
2019-11-12 03:54:15 +0000

    アロー演算子、インデントの修正

--- database/factories/DailyReportFactory.php
+++ database/factories/DailyReportFactory.php
@@ -1,14 +1,16 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Models\DailyReport;
 
 $factory->define(App\Models\DailyReport::class, function (Faker $faker) {
     return [
-                'user_id' => $faker -> numberBetween($min = 4,$max = 4),
-                'title' => 'テスト',
-                'content' => 'テスト' . $faker -> realText($maxNbChars = 200,$indexSize = 2),
-                'reporting_time' => $faker -> dateTimeThisYear($max = 'now',$timezone = null),
-                'created_at' => $faker -> dateTimeThisYear($max = 'now',$timezone = null),
-                'updated_at' => Carbon::now(),
+        // 'user_id' => function() {
+        //   return factory(DailyReport::class)->create()->id;
+        // },
+        'user_id' => $faker->numberBetween($min = 1, $max = 4),
+        'title' => 'テスト',
+        'content' => 'テスト' . $faker->realText($maxNbChars = 200,$indexSize = 5),
+        'reporting_time' => $faker->dateTimeThisYear($max = 'now'),
     ];
 });
