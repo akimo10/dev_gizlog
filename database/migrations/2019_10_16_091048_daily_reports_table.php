13c2a21d7fc2c7321ac5edd610474c91f615c2eb
vagrant <vagrant@homestead>
2019-11-13 19:24:02 +0000

    title,contentカラムの文字数設定

--- database/migrations/2019_10_16_091048_daily_reports_table.php
+++ database/migrations/2019_10_16_091048_daily_reports_table.php
@@ -16,8 +16,8 @@ class DailyReportsTable extends Migration
         Schema::create('daily_reports', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
-            $table->string('title');
-            $table->string('content');
+            $table->string('title', 255);
+            $table->string('content', 255);
             $table->date('reporting_time');
             $table->timestamps();
             $table->softDeletes();
